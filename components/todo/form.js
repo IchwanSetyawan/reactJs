9c9d98e3dce74ce97987fd6cbf751c924a84b314
IchwanSetyawan <43878679+IchwanSetyawan@users.noreply.github.com>
2019-05-11 11:34:00 +0700

    Initial commit

---
+++ components/todo/form.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+//class component
+
+class FormTodo extends Component {
+  constructor(props) {
+    // to access props
+
+    super(props);
+    this.state = {
+      todo: ""
+    };
+  }
+
+  onChangeInput(e) {
+    //this synthetic event is reused for performance reasonsThis
+    e.persist();
+    this.setState({
+      todo: e.target.value
+    });
+  }
+
+  onSubmit() {
+    this.props.submitTodo(this.state.todo);
+    this.setState({
+      todo: ""
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.todo}
+          placeholder="Todo"
+          onChange={e => this.onChangeInput(e)}
+        />
+
+        <button className="add" onClick={() => this.onSubmit()}>
+          +
+        </button>
+      </div>
+    );
+  }
+}
+
+export default FormTodo;
